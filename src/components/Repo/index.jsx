1eb8217aec38eae81ddd2cb06c8cec24b6dc871d
Ariel <arielgomezmendez94@gmail.com>
2023-05-25 10:02:53 -0400

    Create the Repo component

---
+++ src/components/Repo/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./index.css"
+
+const Repo = () => {
+    return (
+        <>
+            <li className='repo'>
+                <h1 className='repoName'>Nombre del repositorio</h1>
+                <p className='description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    <br></br>
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+            </li>
+
+        </>
+    )
+}
+
+export default Repo
