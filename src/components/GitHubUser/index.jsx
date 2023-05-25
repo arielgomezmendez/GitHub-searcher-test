4b13730d5cbc6e5ccd3b79da6c4c7723f475685c
Ariel <arielgomezmendez94@gmail.com>
2023-05-25 14:04:26 -0400

    Add title to the Repo list component

--- src/components/GitHubUser/index.jsx
+++ src/components/GitHubUser/index.jsx
@@ -9,10 +9,17 @@ const GitHubUser = () => {
         //User information
         <section className='mainContainer'>
             <UserInfo />
+
             {/*List of resents repositories */}
-            <ul className='repositoriesList'>
-                <Repo />
-            </ul>
+            <article className='repositoriesList'>
+                <h1 className='title'>Last repositories</h1>
+                <ul >
+                    <Repo />
+                    <Repo />
+                    <Repo />
+                </ul>
+            </article>
+
             <article className='recentSearches'>
                 <h1>Cards</h1>
             </article>
