f67ea54baca214839e25707b24b5aef69bdf7f0c
Ariel <arielgomezmendez94@gmail.com>
2023-05-24 21:11:21 -0400

    Add the github logo in the Search component

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import "./index.css"
+import githubLogo from "../../assets/github-logo.png"
+
 
 const Search = () => {
     return (
         <div className="group">
+            <picture>
+                <img src={githubLogo} alt="GitHub" />
+            </picture>
             <input placeholder="Searth the internet..." type="text" name="text" class="input" />
         </div>
     )
