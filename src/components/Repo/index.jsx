d7bbfe138dee0ff4838859a5d8d5c462600c10ff
Ariel <arielgomezmendez94@gmail.com>
2023-05-25 10:07:54 -0400

    Change color of description

--- src/components/Repo/index.jsx
+++ src/components/Repo/index.jsx
@@ -7,8 +7,7 @@ const Repo = () => {
             <li className='repo'>
                 <h1 className='repoName'>Nombre del repositorio</h1>
                 <p className='description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    <br></br>
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
             </li>
 
         </>
