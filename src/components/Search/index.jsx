603a611422a6f0b1ac39749738e52e9be4c8c331
Ariel <arielgomezmendez94@gmail.com>
2023-05-25 17:37:19 -0400

    Get the user information from the API
    Create the getUser function

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,15 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./index.css"
 import githubLogo from "../../assets/github-logo.png"
 
 
 const Search = () => {
+
+    const [userName, setUser] = useState("")
+    const [userInfo, setUserInfo] = useState("")
+
+    const token = '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'; // personal token to call the API
+
+    //Function to get the user name
+    const onchange = (event) => {
+        setUser(event.target.value)
+    }
+
+    //Function to make the request to the API
+    const getUser = async () => {
+        try {
+            console.log("Funcion obtain user ejecutada")
+            let response = await fetch(
+                `https://api.github.com/users/${userName}`, {
+                headers: {
+                    Authorization: `token ${token}`
+                }
+            });
+
+            let data = await response.json();
+            setUserInfo(data);
+            console.log(userInfo);
+
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    const onclick = (event) => {
+        event.preventDefault()
+        getUser(token, userName)
+    }
+
     return (
         <div className="search">
             <img src={githubLogo} alt="GitHub" className='gitHubLogo' />
             <form>
-                <input placeholder="User name" type="text" name="text" class="input" />
-                <button>Search</button>
+                <input placeholder="User name" type="text" name="text" className="input" onChange={onchange} />
+                <button onClick={onclick}>Search</button>
             </form>
 
         </div>
