15bda9c5b07a523fcdc1813ed2b30f47ae3c1c0f
Ariel <arielgomezmendez94@gmail.com>
2023-05-27 16:29:06 -0400

    Create the BackIllustration component

---
+++ src/components/BackIllustration/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import backIllustration from "../../assets/illustration-github.png"
+
+const BackIllustration = () => {
+    return (
+        <>
+            <img src={backIllustration} alt='Github Illustration' />
+        </>
+    )
+}
+
+export default BackIllustration
--- src/components/GitHubUser/index.jsx
+++ src/components/GitHubUser/index.jsx
@@ -3,6 +3,7 @@ import "./index.css"
 import Repo from '../Repo';
 import UserInfo from '../UserInfo';
 import MyContext from '../../context/MyContext';
+import BackIllustration from '../BackIllustration';
 
 
 const GitHubUser = () => {
@@ -11,13 +12,15 @@ const GitHubUser = () => {
     return (
         //User information
         <section className='mainContainer'>
+            <BackIllustration />
             <UserInfo />
 
             {/*List of resents repositories */}
             <article className='repositoriesList'>
                 <h1 className='title'>Last repositories</h1>
-                <ul >{/* Array.isArray(userRepos) checks if userRepos is an array before executing the map method.
-                 If userRepos is not an array, the code inside the map will not execute and avoid the error. */}
+                <ul >
+                    {/* Array.isArray(userRepos) checks if userRepos is an array before executing the map method.
+                    If userRepos is not an array, the code inside the map will not execute and avoid the error. */}
                     {Array.isArray(userRepos) && userRepos.map((repo, index) => (
                         <Repo key={repo.id} name={repo.name} description={repo.description} />
                     ))}
