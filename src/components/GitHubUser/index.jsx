bd96d4acd91594ceb030051df1d3cd44661ec86b
Ariel <arielgomezmendez94@gmail.com>
2023-05-27 19:33:29 -0400

    Create the ReposList component

--- src/components/GitHubUser/index.jsx
+++ src/components/GitHubUser/index.jsx
@@ -1,9 +1,9 @@
 import React, { useContext } from 'react'
 import "./index.css"
-import Repo from '../Repo';
 import UserInfo from '../UserInfo';
 import MyContext from '../../context/MyContext';
 import BackIllustration from '../BackIllustration';
+import ReposList from '../ReposList';
 
 
 const GitHubUser = () => {
@@ -12,20 +12,9 @@ const GitHubUser = () => {
     return (
         //User information
         <section className='mainContainer'>
-            <BackIllustration />
-            <UserInfo />
+            {!Array.isArray(userRepos) ? <BackIllustration /> : <UserInfo />}
+            <ReposList />
 
-            {/*List of resents repositories */}
-            <article className='repositoriesList'>
-                <h1 className='title'>Last repositories</h1>
-                <ul >
-                    {/* Array.isArray(userRepos) checks if userRepos is an array before executing the map method.
-                    If userRepos is not an array, the code inside the map will not execute and avoid the error. */}
-                    {Array.isArray(userRepos) && userRepos.map((repo, index) => (
-                        <Repo key={repo.id} name={repo.name} description={repo.description} />
-                    ))}
-                </ul>
-            </article>
         </section>
     )
 }
---
+++ src/components/ReposList/index.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react'
+import Repo from '../Repo'
+import MyContext from '../../context/MyContext'
+
+const ReposList = () => {
+    const { userRepos } = useContext(MyContext)
+    console.log("userrepo", userRepos)
+    return (
+        <div>
+            {/*List of resents repositories */}
+            <article className='repositoriesList'>
+                <h1 className='title'>Last repositories</h1>
+                <ul >
+                    {/* Array.isArray(userRepos) checks if userRepos is an array before executing the map method.
+                    If userRepos is not an array, the code inside the map will not execute and avoid the error. */}
+                    {Array.isArray(userRepos) && userRepos.map((repo, index) => (
+                        <Repo key={repo.id} name={repo.name} description={repo.description} />
+                    ))}
+                </ul>
+            </article>
+        </div>
+    )
+}
+
+export default ReposList
