c194766674d893451b72336087ab8c56ccf22e2d
Ariel <arielgomezmendez94@gmail.com>
2023-05-27 20:06:20 -0400

    Add styles to the BackIllustration component
    Styles: elements size, position, colors

--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import Repo from '../Repo'
 import MyContext from '../../context/MyContext'
+import "./index.css"
 
 const ReposList = () => {
     const { userRepos } = useContext(MyContext)
