2ea2710620467c8d704eb320c884a13ce79bb3e0
Ariel <arielgomezmendez94@gmail.com>
2023-05-27 20:01:19 -0400

    Change the position and size of elements from BackIllustration component

--- src/components/BackIllustration/index.jsx
+++ src/components/BackIllustration/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import backIllustration from "../../assets/illustration-github.png"
-import "./index"
+import "./index.css"
 
 const BackIllustration = () => {
     return (
-        <div className=''>
+        <div className='backIllustration'>
             <h1>Search a GitHub user</h1>
             <img src={backIllustration} alt='Github Illustration' />
         </div>
